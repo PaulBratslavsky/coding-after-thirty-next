ca121887ff934dcb5f1454dfa17b74848f614c65
paul <paul@Mac.localdomain>
2024-12-18 19:52:21 -0600

    fix dynamic page typing

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/paul/programing/yt-projects/coding-after-thirty-next/src/app/layout":["static/media/463dafcda517f24f-s.p.woff","static/media/4473ecc91f70f139-s.p.woff"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
