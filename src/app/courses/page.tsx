c7eef5980956307735b6f71f2777ebcc385145ae
paul <paul@Mac.localdomain>
2025-02-27 18:02:02 -0600

    added courses page

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,63 @@
+import type { ImageProps } from "@/types";
+import { notFound } from "next/navigation";
+
+import { getAllCourses } from "@/data/loaders";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselPrevious,
+  CarouselNext,
+} from "@/components/ui/carousel";
+
+import { SectionLayout } from "@/components/custom/section-layout";
+import { CourseItem } from "@/components/custom/course-item";
+
+export interface CourseData {
+  id: number;
+  documentId: string;
+  title: string;
+  description: string;
+  isPremium: boolean;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  slug: string;
+  content: string | null;
+  image: ImageProps;
+}
+
+async function loader() {
+  const data = await getAllCourses();
+  if (!data?.data) notFound();
+
+  return {
+    headerData: { ...mockData },
+    courseData: data.data,
+  };
+}
+
+export default async function CoursesRoute() {
+  const { headerData, courseData } = await loader();
+
+  return (
+    <SectionLayout {...headerData}>
+      <Carousel
+        opts={{ align: "start", loop: true }}
+        className="mt-6 w-full px-4 xl:px-0"
+      >
+        <CarouselPrevious className="-left-6 size-7 xl:-left-12 xl:size-8" />
+        <CarouselContent className="pb-4">
+          {courseData.map((course) => <CourseItem course={course as CourseData} key={course.documentId} />)}
+        </CarouselContent>
+        <CarouselNext className="-right-6 size-7 xl:-right-12 xl:size-8" />
+      </Carousel>
+    </SectionLayout>
+  );
+}
+
+const mockData = {
+  subHeading: "Expand Your Knowledge",
+  heading: "Our Featured Courses",
+  text: "Whether you're a beginner or an expert, our curated courses are designed to help you level up your skills.",
+};
