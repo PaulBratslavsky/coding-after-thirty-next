9a66aa2c9ce4bba3771659451cd206356d2152a8
paul <paul@Mac.localdomain>
2025-02-28 23:34:19 -0600

    update to puvlic env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getStrapiURL() {
-  return process.env.STRAPI_API_URL ?? "http://localhost:1337";
+  return process.env.NEXT_PUBLIC_STRAPI_API_URL ?? "http://localhost:1337";
 }
 
 export function getStrapiMedia(url: string | null) {
