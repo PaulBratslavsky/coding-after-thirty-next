c72cdfd322ce9423f2f59529a90de0ae277cca6f
paul <paul@dev.local>
2025-06-06 13:39:23 -0500

    testing

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -2,5 +2,6 @@ import { getStrapiURL } from "@/lib/utils";
 import { strapi } from "@strapi/client";
 
 const BASE_API_URL = getStrapiURL() + "/api";
+console.log(BASE_API_URL);
 const sdk = strapi({ baseURL: BASE_API_URL, auth: process.env.STRAPI_API_KEY });
 export { sdk };
