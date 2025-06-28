2254a31a52a4e77f435c631297311941fde9a33b
home-server <home-server@Mac.localdomain>
2025-06-27 22:31:35 -0500

    fix app breaking when no profile is found

--- src/app/(protected)/dashboard/profile/page.tsx
+++ src/app/(protected)/dashboard/profile/page.tsx
@@ -1,32 +1,32 @@
-import { StrapiUserProfileData, StrapiUserData } from "@/types/user";
+import { StrapiUserProfileData } from "@/types/user";
 import { getUserMeLoader, getUserProfileByIdLoader } from "@/lib/services/user";
-import { notFound } from "next/navigation";
 
-async function loader(): Promise<{ profile: StrapiUserProfileData, user: StrapiUserData }> {
-  const response = await getUserMeLoader();
-  
-  console.log(response)
-  const profileUserId = response?.data?.userProfile?.documentId;
+async function loader(): Promise<{ profile: StrapiUserProfileData | null }> {
+  const userResponse = await getUserMeLoader();
+  const profileUserId = userResponse?.data?.userProfile?.documentId;
 
-  if (!profileUserId) return notFound();
+  let profile = null;
 
-  try {
-    const response = await getUserProfileByIdLoader(profileUserId) 
-    return { profile: response?.data?.data || null,
-      user: response.data
-    };
-  } catch (error) {
-    console.error(error);
-    throw error; 
+  if (profileUserId) {
+    try {
+      const response = await getUserProfileByIdLoader(profileUserId) 
+      profile = response?.data;
+    } catch (error) {
+      console.error(error);
+      throw error; 
+    }
   }
+
+  return { 
+      profile: profile,
+    };
 }
 
 export default async function ProfileRoute() {
-  const { profile, user } = await loader();
+  const { profile } = await loader();
   return (
     <div className="flex flex-col gap-4 p-4">
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-        <pre>{JSON.stringify(profile, null, 2)}</pre>
+      { profile ? <pre>{JSON.stringify(profile, null, 2)}</pre> : <pre>No profile found.</pre>}
     </div>
   );
 }
